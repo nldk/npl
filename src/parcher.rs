eeebd4dfe03c4e553c350c73f99a22c4de440907
Niel <niel.dekeyser@gmail.com>
2025-08-25 11:28:03 +0200

    adding custom split function

--- src/parcher.rs
+++ src/parcher.rs
@@ -7,14 +7,13 @@ pub fn parch(file:String,program: &mut Program,debug:bool) ->Program{
         let instruction_and_arguments = i.split_once(" ").unwrap();
         let (instruction,arguments) = instruction_and_arguments;
         let arguments = arguments.to_string();
-        let arguments = arguments.split(",").collect::<Vec<&str>>();
-        let mut arguments_to_put_in_the_line:Vec<String> = Vec::new();
-        for e in arguments{
-            arguments_to_put_in_the_line.push(e.to_string());
+        let arguments = split_Arguments(arguments,debug);
+        if debug{
+            println!("{:?}",arguments);
         }
         program_lines.push(Line{
             instruction:instruction.to_string(),
-            arguments: arguments_to_put_in_the_line,
+            arguments: arguments,
         })
     }
     if debug{
@@ -22,4 +21,36 @@ pub fn parch(file:String,program: &mut Program,debug:bool) ->Program{
     }
     program.lines = program_lines;
     program.to_owned()
+}
+
+fn split_Arguments(string:String,debug:bool)->Vec<String>{
+    if debug {
+        println!("string{:?}",string);
+    }
+    let mut arguments:Vec<String> = Vec::new();
+    let mut isString = false;
+    let mut argument:String = String::new();
+    for i in string.chars(){
+        match i { 
+            '"' =>{isString=!isString;},
+            ','=>{
+                if debug{
+                    println!("isSting:{:?},{}",isString,i);
+                }
+                if isString{
+                    argument.push(i);
+                }else {
+                    if debug{
+                        println!("argument{:?}",argument);
+                        
+                    }
+                    arguments.push(argument.clone());
+                    argument = String::new();
+                }
+            },
+            _=>{argument.push(i);}
+        }
+    }
+    arguments.push(argument);
+    arguments
 }
